f82cfa8a6c5deb8b11864f9efb3fd8d0a53a0dfe
GoldenZero7 <52763835+GoldenZero7@users.noreply.github.com>
2022-04-05 16:05:14 -0700

    About Page

--- routes/views.js
+++ routes/views.js
@@ -1,12 +1,18 @@
-const H = {}
+const H = {};
 
-H.index = function(req, res){
-	return res.render('../views/index', {
-	  	page_path: '/',
-	  	page_title: 'home',
-	  	html_title: 'HOME'
-	})
-}
+H.index = function (req, res) {
+  return res.render("../views/index", {
+    page_path: "/",
+    page_title: "home",
+    html_title: "HOME",
+  });
+};
+H.about = function (req, res) {
+  return res.render("../views/about", {
+    page_path: "/about",
+    page_title: "about",
+    html_title: "ABOUT",
+  });
+};
 
 module.exports = H;
-
